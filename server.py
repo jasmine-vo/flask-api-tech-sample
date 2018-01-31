ca61905e1bf7ef2955504abb60d09ab9e0bcbd26
Jasmine Vo <jasmine@Jasmines-MacBook-Pro.local>
2018-01-30 20:11:21 -0800

    Handle case if the request method is GET instead of POST

--- server.py
+++ server.py
@@ -1,18 +1,23 @@
 from flask import Flask, request, jsonify
+
 app = Flask(__name__)
 
-@app.route('/test', methods=["POST"])
+@app.route('/test', methods=["POST", "GET"])
 def test():
     """Takes in two numbers as arguments and returns the sum in JSON format"""
 
-    json_request = request.get_json()
+    if request.method == "POST":
+        json_request = request.get_json()
 
-    x = json_request.get("x")
-    y = json_request.get("y")
+        x = json_request.get("x")
+        y = json_request.get("y")
 
-    sum_nums = {"sum": x + y}
+        response = {"sum": x + y}
 
-    return jsonify(sum_nums)
+    else:
+        response = {"message": "The method is not allowed for the requested URL."}
+
+    return jsonify(response)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, port=5000, host='0.0.0.0')
